2cb344835db4a461d41bb03a392bbf4cf7af47ca
Pavel-Pro <proskpavel@gmail.com>
2020-11-26 19:02:01 +0200

    Адаптация
    Концовка адаптпации 5 первых экранов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$('.burger__link').click(function() {
-		$('.menu').slideToggle('500')
+	$('.burger-nav').click(function() {
+		$('.nav__items').slideToggle('500')
 		$('.burger:first-child').toggleClass('top')
 		$('.burger:nth-child(2)').toggleClass('middle')
 		$('.burger:last-child').toggleClass('bottom');
